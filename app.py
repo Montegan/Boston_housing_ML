141ccca205cb7c76681a4d9c4acd20bf8279b7b9
Simon Fasil <simonfasil78@gmail.com>
2025-05-26 17:57:10 -0500

    added form submission in home.html and added predict endpoint in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, app, jsonify, url_for, render_template  # type: ignore
 import numpy as np
 import pandas as pd
 
@@ -19,7 +19,7 @@ def second():
     return render_template("second.html")
 
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict_api", methods=['POST'])
 def predictions():
     data = request.json["data"]
     print(data)
@@ -28,6 +28,16 @@ def predictions():
     reg_output = regmodel.predict(data_scalled)
     print(reg_output[0])
     return jsonify(reg_output[0])
+
+
+@app.route('/predict', methods=["POST"])
+def make_prediction():
+    data = [float(x) for x in request.form.values()]
+    np_data = np.array(data).reshape(1, -1)
+    scalled_data = scaller.transform(np_data)
+    predicted_price = regmodel.predict(scalled_data)
+    print(np_data.shape)
+    return render_template("home.html", predicted="The predicted value is {}".format(predicted_price[0]))
 
 
 if __name__ == "__main__":
